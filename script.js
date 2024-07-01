2cd8c7365de10a82e318bcbb29575dcce3f67691
Pepperzero <chimichanga91@hotmail.it>
2024-07-01 14:14:01 +0200

    update script.js

--- script.js
+++ script.js
@@ -127,6 +127,7 @@ animateWords();
 
 // items.on("click", toggleAccordion);
 
+//////// DROPDOWN BEING
 $("[dropdown]").on("click", function () {
   // Find the parent .being_item
   var parentItem = $(this).closest(".being_item");
@@ -149,4 +150,102 @@ $("[dropdown]").on("click", function () {
   } else {
     dropdownComponent.animate({ height: 0 }, 300);
   }
+});
+
+//////// DROPDOWN TIME
+// $("[dropdown-time]").on("click", function () {
+//   // Find the parent .being_item
+//   var parentItem = $(this).closest(".time_distance-wrap");
+
+//   // Find the .dropdown_component within this parent
+//   var dropdownComponent = parentItem.find(".time_distance-cards");
+
+//   console.log("clicked");
+//   if (dropdownComponent.height() === 0) {
+//     dropdownComponent.css("height", "auto");
+//     var autoHeight = dropdownComponent.height();
+//     dropdownComponent.css("height", "0");
+//     dropdownComponent.animate({ height: autoHeight }, 300);
+//   } else {
+//     dropdownComponent.animate({ height: 0 }, 300);
+//   }
+// });
+
+//Menu animation
+window.addEventListener("DOMContentLoaded", (event) => {
+  let drop = $(".time_distance-wrap");
+
+  drop.each(function (index) {
+    let dropTrigger = $(this).find(".time_distance-trigger");
+    let dropContent = $(this).find(".time_distance-cards");
+    let dropImage = $(this).find(".time_distance_img-wrap");
+    let dropInfo = $(this).find(".timer_distance_txt_inner-wrap");
+    // let menuBackground = $(".menu_background");
+    // let lines = $(".nav_button_line");
+
+    let openDropdown = gsap.timeline({
+      paused: true,
+      defaults: {
+        duration: 1,
+        ease: "power2.inOut",
+      },
+      onReverseComplete: () => {
+        dropTrigger.attr("aria-label", "Open Main Menu");
+      },
+      onComplete: () => {
+        //dropContent.find("button").first().focus();
+        dropTrigger.attr("aria-label", "Close Main Menu");
+      },
+    });
+    openDropdown.to(dropImage, {
+      height: 0,
+      autoAlpha: 0,
+      display: "none",
+    });
+    openDropdown.to(
+      dropInfo,
+      {
+        height: 0,
+        autoAlpha: 0,
+        display: "none",
+      },
+      "<"
+    );
+    openDropdown.to(
+      dropTrigger,
+      {
+        //duration: 0.2,
+        height: "6.6rem",
+      },
+      "<"
+    );
+    openDropdown.to(
+      dropContent,
+      {
+        height: "auto",
+        autoAlpha: 1,
+        display: "block",
+      }
+      //"<"
+    );
+    // openDropdown.from(dropContent, { y: "-100%" }, '-=50%');
+    // openDropdown.from(menuBackground, { opacity: 0 }, "<");
+    // openDropdown.to(lines.eq(0), { y: 4, rotate: 45 }, "<");
+    // openDropdown.to(lines.eq(1), { y: -4, rotate: -45 }, "<");
+
+    dropTrigger.on("click", function () {
+      if (openDropdown.progress() === 0) {
+        openDropdown.play();
+      } else {
+        openDropdown.reverse();
+        dropTrigger.attr("aria-label", "Open Main Menu");
+      }
+    });
+    // menuBackground.on("click", function () {
+    //   openDropdown.reverse();
+    // });
+    // $(document).on("keydown", function (e) {
+    //   if (e.key === "Escape") openDropdown.reverse();
+    // });
+  });
 });
